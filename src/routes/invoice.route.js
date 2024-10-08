ac9670a64e432df91997e664bc1112b3d715986e
HuynhAnhKhoi2052001 <anhkhoi2052001@gmail.com>
2024-10-08 23:18:01 +0700

    fix create invoice with appointmentId from path params

--- src/routes/invoice.route.js
+++ src/routes/invoice.route.js
@@ -4,6 +4,6 @@ const {
 } = require("../controllers/invoice.controller");
 const auth = require("../middlewares/auth.middleware");
 const router = require("express").Router();
-router.post("/create", auth(["admin", "staff"]), saveInvoice);
+router.post("/create/:appointmentId", auth(["admin", "staff"]), saveInvoice);
 router.get("/get-all", auth(["admin", "staff"]), getAllInvoice);
 module.exports = router;
